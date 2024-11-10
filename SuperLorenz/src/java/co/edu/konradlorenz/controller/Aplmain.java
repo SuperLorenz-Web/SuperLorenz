0972bb19a8b844dab2da80a9cca118eeee0363b8
13rian Vargas <bsvargasc13@gmail.com>
2024-11-10 13:13:50 -0500

    NEW - Controller.
    >> Controller en blanco por si se usa en el futuro.

--- SuperLorenz/src/java/co/edu/konradlorenz/controller/Aplmain.java
+++ SuperLorenz/src/java/co/edu/konradlorenz/controller/Aplmain.java
@@ -2,21 +2,8 @@ package co.edu.konradlorenz.controller;
 
 public class Aplmain {
     public static void main(String[] args) {
-        
-        /*/ <- Quitar * entre barras para descomentar bloque.
-        ControllerAdmin controllerAdmin = new ControllerAdmin();
-        controllerAdmin.run();
-        
-        ControllerEmpleado controllerEmpleado = new ControllerEmpleado();
-        controllerEmpleado.run();
-        
-        ControllerCliente controllerCliente = new ControllerCliente();
-        controllerCliente.run();
-        
-        ControllerProveedor controllerProveedor = new ControllerProveedor();
-        controllerProveedor.run();
-        //*/
-        
+        Controller controller = new Controller();
+        controller.run();
     }
     //main
 }
