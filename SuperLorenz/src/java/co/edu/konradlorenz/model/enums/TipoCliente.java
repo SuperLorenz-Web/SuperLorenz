7c0f37ba32c27a9acd8330b1e977a76ef911b84b
Shar <montenegrosharik6@gmail.com>
2024-11-21 13:53:20 -0500

    Finalización Enums
    Añadí los datos a 2 enums que me faltaron (Mario)

--- SuperLorenz/src/java/co/edu/konradlorenz/model/enums/TipoCliente.java
+++ SuperLorenz/src/java/co/edu/konradlorenz/model/enums/TipoCliente.java
@@ -1,5 +1,5 @@
 package co.edu.konradlorenz.model.enums;
 
 public enum TipoCliente {
-    
+    NORMAL, FRECUENTE, EMPRESARIAL, PREFERENCIAL
 }
--- SuperLorenz/src/java/co/edu/konradlorenz/model/enums/TipoDocumento.java
+++ SuperLorenz/src/java/co/edu/konradlorenz/model/enums/TipoDocumento.java
@@ -1,5 +1,5 @@
 package co.edu.konradlorenz.model.enums;
 
 public enum TipoDocumento {
-    
+    RC, CC, CE, PEP, PPT, PP, NIT
 }
